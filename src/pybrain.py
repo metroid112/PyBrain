b8781200307064cdf92e892594f18e3e1b2acb29
metroid112 <federico.aguilera112@gmail.com>
2019-11-13 10:27:22 -0300

    fahrenheit kelvin conversion

--- src/pybrain.py
+++ src/pybrain.py
@@ -1,6 +1,17 @@
 import numpy as np
 import tensorflow as tf
+import os
 
-fahrenheit_input = np.array([-40, 14, 32, 46])
-kelvin_output = np.array([233.15, 263.15, 273.15, 280.928])
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+input_data = np.array([-40.0, 14.0, 32.0, 46.0])
+output_data = np.array([233.15, 263.15, 273.15, 280.928])
+
+layers = tf.keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([layers])
+
+model.compile(loss=tf.losses.MeanSquaredError, optimizer=tf.keras.optimizers.Adam(.1))
+history = model.fit(input_data, output_data, epochs=9000)
+
+for i, element in enumerate(input_data):
+    print(f'F {element} - K {output_data[i]} - M {model.predict([element])}')
