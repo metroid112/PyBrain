4927e0888c7868841a1b0e20e5403ff642ff160c
metroid112 <federico.aguilera112@gmail.com>
2019-11-13 14:24:00 -0300

    fix loss

--- src/pybrain.py
+++ src/pybrain.py
@@ -10,8 +10,9 @@ output_data = np.array([233.15, 263.15, 273.15, 280.928])
 layers = tf.keras.layers.Dense(units=1, input_shape=[1])
 model = tf.keras.Sequential([layers])
 
-model.compile(loss=tf.losses.MeanSquaredError, optimizer=tf.keras.optimizers.Adam(.1))
+model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(.1))
 history = model.fit(input_data, output_data, epochs=9000)
 
 for i, element in enumerate(input_data):
     print(f'F {element} - K {output_data[i]} - M {model.predict([element])}')
+
