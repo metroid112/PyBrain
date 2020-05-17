047151a6d0936c1b08f67ddc6ec2c6007f989c2c
metroid112 <federico.aguilera112@gmail.com>
2020-05-17 01:33:59 -0300

    activation

--- src/pybrain.py
+++ src/pybrain.py
@@ -11,9 +11,18 @@ class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.output = []
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def __init__(self):
+        self.output = []
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
 
 layer1 = Layer_Dense(4, 5)
@@ -21,3 +30,7 @@ layer2 = Layer_Dense(5, 2)
 
 layer1.forward(X)
 layer2.forward(layer1.output)
+
+print(layer1.output)
+
+print(layer2.output)
